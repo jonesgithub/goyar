45b93613b7d3c99543676bfa4d9d05cfeec34763
neverlee <neverleee@qq.com>
2016-03-20 13:46:25 +0800

    remove the copied li.. info

--- client.go
+++ client.go
@@ -1,8 +1,4 @@
-// Copyright 2010 The Go Authors.  All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package jsonrpc implements a JSON-RPC ClientCodec and ServerCodec
+// Package gorpc implements a YAR-RPC ClientCodec and ServerCodec
 // for the rpc package.
 package goyar
 
